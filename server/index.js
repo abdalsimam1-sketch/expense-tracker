dc9258377cc52d0e24fcd64d5270c236da6f2bda
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-06-04 14:44:22 +0100

    chore : setup entry point

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const app = express();
+require("dotenv/config");
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errorHandler");
+require("./config/connectDB");
+
+const port = process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+
+//security
+app.use(cors());
+app.use(helmet());
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  }),
+);
+
+//routes
+
+//error handling
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}....`);
+});
