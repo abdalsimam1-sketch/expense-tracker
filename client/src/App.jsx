13c0d47f3e28050bada00f55b30fb774bc76a95d
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-06-05 21:56:50 +0100

    feat : mount routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import { Auth } from "./pages/Auth";
+import { Home } from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { ProtectedRoutes } from "./components/ProtectedRoutes";
 
 const App = () => {
   return (
     <div>
-      <Auth />
+      <Routes>
+        <Route path="/" element={<Auth />}></Route>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoutes>
+              <Home></Home>
+            </ProtectedRoutes>
+          }
+        ></Route>
+      </Routes>
     </div>
   );
 };
