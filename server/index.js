d994278037b4dc33aeb3bd257421e58c3aedb5f1
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-06-05 11:53:56 +0100

    feat : mount expenses routes in entry point

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const notFound = require("./middleware/notFound");
 const errorHandler = require("./middleware/errorHandler");
 require("./config/connectDB");
 const authRouter = require("./routes/authRoutes");
+const expensesRouter = require("./routes/expensesRoutes");
 
 const port = process.env.PORT || 3000;
 
@@ -26,6 +27,7 @@ app.use(
 
 //routes
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/expenses", expensesRouter);
 
 //error handling
 app.use(notFound);
